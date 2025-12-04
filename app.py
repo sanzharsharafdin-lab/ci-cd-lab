f5731f0276950b287d394432f377deec815639cd
sanzharsharafdin-lab <sanzharsharafdin@gmail.com>
2025-12-04 19:28:13 +0600

    Convert app to use Flask framework

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello from Python app!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Python Flask App!"
+
+if __name__ == "__main__":
+    app.run()
